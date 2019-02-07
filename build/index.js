ab3ec0b9fbc1941786ee26c93e555ca5920c27e3
smazad1212 <smazad1212@gmail.com>
2019-02-07 13:29:15 +0600

    css not working in build fix

--- build/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = () => {
-  require('./ReactOfflineNotify');
-  require('./ReactOfflineNotify.css');
-};
--- src/components/ReactOfflineNotify/index.js
+++ src/components/ReactOfflineNotify/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react"
 
-import "./styles.css"
+import "../../styles.css"
 
 export default class Index extends Component {
   state = {
